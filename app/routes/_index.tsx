69514e60d743b4b03e5ac3888720391add1a6464
Karl <karlcereno@gmail.com>
2023-11-28 10:53:51 -0500

    Set up loader

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,38 +9,12 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  return (
-    <div className="bg-red-300">
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+  const data = useLoaderData<typeof loader>();
+  console.log('Index ~ data:', data);
+
+  return <div className="bg-red-300">{data.message}</div>;
+}
+
+export function loader() {
+  return { message: 'Hello from loader!' };
 }
